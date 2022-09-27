f9bf3e9cd4fe87204f63085c363812a962c9ce87
abieko <113584008+abieko@users.noreply.github.com>
2022-09-27 21:36:23 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,42 @@
 import './App.css';
-import { Card, CardContent, Grid, List } from "@mui/material";
-import { useState } from "react";
-import Header from "./components/Header";
-import Contact from "./components/Contact";
-import ContactForm from "./components/ContactForm";
+import Header from './components/Header'
+import ContactForm from './components/ContactForm'
+import Contact from './components/Contact'
 
 // Uncomment untuk memuat daftar kontak
-// import contactsJSON from './data/contacts.json';
-import contactJson from './data/contacts.json'
+import contactsJSON from './data/contacts.json';
+import {useState} from "react";
+import {List} from "@mui/material";
+
 const App = () => {
-  // Masukkan Header dan lakukan map untuk Contact ke dalam div App
-  // untuk membuat daftar kontak bisa menggunakan MUI list
-  // https://mui.com/material-ui/react-list/#folder-list
+    // Masukkan Header dan lakukan map untuk Contact ke dalam div App
+    // untuk membuat daftar kontak bisa menggunakan MUI list
+    // https://mui.com/material-ui/react-list/#folder-list
 
-  // Masukkan contacts yang sudah didapat dalam JSON sebagai initial state
-    const [contacts, setContacts] = useState(contactJson)
-  // Buatlah handler untuk menambahkan kontak baru yang akan dikirim ke ContactForm
-    const handleNewContact = (value) => setContacts([...contacts, value]);
+    // Masukkan contacts yang sudah didapat dalam JSON sebagai initial state
+    // Buatlah handler untuk menambahkan kontak baru yang akan dikirim ke ContactForm
+    const [contacts, setContacts] = useState(contactsJSON)
+    const onAddContactHandler = (data) => {
+        setContacts(state => [...state, data])
+    }
 
-  return (
-    <div className="App">
-      <Header />
-        <Grid container style={{ padding:"30px 0" }}>
-            <Grid item md={6} sm={12}>
-                <ContactForm addContact={(value) => handleNewContact(value)} />
-            </Grid>
-            <Grid item md={6} sm={12}>
-                <Card sx={{ backgroundColor: "#DBF6F0" }}>
-                    <CardContent>
-                        <List>
-                            {contacts.map((contact, index) => (
-                                <Contact key={index} data={contact} />
-                            ))}
-                        </List>
-                    </CardContent>
-                </Card>
-            </Grid>
-        </Grid>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <div className="contact-container">
+                <ContactForm AddContact={onAddContactHandler}/>
+                <div>
+                    <List dense={true} sx={{minWidth: 500, bgcolor: 'background.paper', height: 'fit-content'}}>
+                        {
+                            contacts.map((contact, index) => (
+                                <Contact key={index} data={contact}/>
+                            ))
+                        }
+                    </List>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default App;
